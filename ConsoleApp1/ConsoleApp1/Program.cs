83fea977c3e342740c2f3fb87262617d1bb41132
LingxiaChen <Lingxia.Chen@microsoft.com>
2019-03-13 10:54:23 +0800

    update

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Data;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 [Serializable()]
 class TestClassA
 {
-    private TestClassA testClassAInA;
+    private readonly TestClassA TestClassAInA;
 
     public void TestMethod()
     {
@@ -13,9 +17,20 @@ class TestClassA
 [Serializable()]
 class TestClassB
 {
-    private TestClassA testClassAInB;
+    private readonly TestClassA TestClassAInB;
 
     public void TestMethod()
     {
+    }
+}
+
+class TestClassC
+{
+    public static void Main()
+    {
+        byte[] buffer = new byte[] { 1, 2, 3 };
+        MemoryStream memStream = new MemoryStream(buffer);
+        BinaryFormatter bf = new BinaryFormatter(null, new StreamingContext(StreamingContextStates.All));
+        DataTable dt = (DataTable)bf.Deserialize(memStream);
     }
 }
