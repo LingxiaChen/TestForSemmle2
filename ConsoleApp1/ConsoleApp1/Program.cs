a0b9da9d1c892d3458bde083725dcbd3d49273d3
LingxiaChen <Lingxia.Chen@microsoft.com>
2019-03-13 11:14:13 +0800

    update

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,26 +1,51 @@
 ﻿using System;
+using System.Collections;
 using System.Data;
 using System.IO;
+using System.Messaging;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
-[Serializable()]
-class TestClassA
-{
-    private readonly TestClassA TestClassAInA;
+namespace System.Messaging{
+    public interface IMessageFormatter : ICloneable
+    { }
 
-    public void TestMethod()
+    public class MessageQueue : System.ComponentModel.Component, System.Collections.IEnumerable
     {
+        public MessageQueue(string message)
+        {
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Messaging.IMessageFormatter Formatter { get; set; }
+
+        public System.Messaging.Message Receive()
+        {
+            return null;
+        }
     }
-}
 
-[Serializable()]
-class TestClassB
-{
-    private readonly TestClassA TestClassAInB;
-
-    public void TestMethod()
+    public class BinaryMessageFormatter : ICloneable, System.Messaging.IMessageFormatter
     {
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Message : System.ComponentModel.Component
+    {
+        public object Body { get; set; }
+    }
+
+    public sealed class Bitmap
+    {
+        public void Save(string filename)
+        { }
     }
 }
 
@@ -28,9 +53,16 @@ class TestClassC
 {
     public static void Main()
     {
-        byte[] buffer = new byte[] { 1, 2, 3 };
-        MemoryStream memStream = new MemoryStream(buffer);
-        BinaryFormatter bf = new BinaryFormatter(null, new StreamingContext(StreamingContextStates.All));
-        DataTable dt = (DataTable)bf.Deserialize(memStream);
+        MessageQueue myQueue = new MessageQueue(".\\myQueue");
+
+        // Set the formatter to indicate body contains an Order.
+        myQueue.Formatter = new BinaryMessageFormatter();
+
+        // Receive and format the message. 
+        System.Messaging.Message myMessage = myQueue.Receive();
+        Bitmap myImage = (Bitmap)myMessage.Body;
+
+        // This will be saved in the \bin\debug or \bin\retail folder.
+        myImage.Save("ReceivedImage.bmp");
     }
 }
